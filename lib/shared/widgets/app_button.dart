5f6542ed4874f77b542bdf3e81af76c7b9e996ea
Jeremiah <jeremiaholuwafemi16@gmail.com>
2026-09-13 07:03:56 +0100

    fix(ui): remove unused theme variable from AppButton

--- lib/shared/widgets/app_button.dart
+++ lib/shared/widgets/app_button.dart
@@ -22,8 +22,6 @@ class AppButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    
     Widget buttonChild = isLoading
         ? const SizedBox(
             height: 20,
