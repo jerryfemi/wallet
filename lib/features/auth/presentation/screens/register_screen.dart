dba89397bc08740fb7fe0702d4f9abd1381692f6
Jeremiah <jeremiaholuwafemi16@gmail.com>
2026-09-14 18:39:46 +0100

    feat: trigger PremiumSuccessSheet on successful registration

--- lib/features/auth/presentation/screens/register_screen.dart
+++ lib/features/auth/presentation/screens/register_screen.dart
@@ -5,6 +5,7 @@ import 'package:go_router/go_router.dart';
 import '../../../../shared/widgets/app_button.dart';
 import '../../../../shared/widgets/app_text_field.dart';
 import '../../../../shared/widgets/premium_error_sheet.dart';
+import '../../../../shared/widgets/premium_success_sheet.dart';
 import '../../domain/utils/auth_exception_mapper.dart';
 import '../providers/auth_provider.dart';
 
@@ -31,6 +32,15 @@ class RegisterScreen extends HookConsumerWidget {
             title: 'Registration Failed',
             message: message,
           );
+        },
+        data: (_) {
+          if (previous?.isLoading == true) {
+            PremiumSuccessSheet.show(
+              context,
+              title: 'Account Created!',
+              message: 'You have successfully signed up.',
+            );
+          }
         },
       );
     });
