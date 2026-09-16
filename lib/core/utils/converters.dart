c7cfd52ef97fcf64ef3ca56d5d7275a1a0bf0a5e
Jeremiah <jeremiaholuwafemi16@gmail.com>
2026-09-16 23:01:32 +0100

    feat(core): add timestamp converter for firestore date parsing

---
+++ lib/core/utils/converters.dart
@@ -0,0 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+class TimestampConverter implements JsonConverter<DateTime, dynamic> {
+  const TimestampConverter();
+
+  @override
+  DateTime fromJson(dynamic json) {
+    if (json is Timestamp) return json.toDate();
+    if (json is String) return DateTime.parse(json);
+    if (json is int) return DateTime.fromMillisecondsSinceEpoch(json);
+    return DateTime.now();
+  }
+
+  @override
+  dynamic toJson(DateTime object) => Timestamp.fromDate(object);
+}
