442b5446b16fbc04a83c7becddd759f4a606c34d
Jeremiah <jeremiaholuwafemi16@gmail.com>
2026-09-17 02:04:45 +0100

    chore: regenerate asset_model.g.dart after converter change

--- lib/features/wallet/data/models/asset_model.g.dart
+++ lib/features/wallet/data/models/asset_model.g.dart
@@ -9,12 +9,12 @@ part of 'asset_model.dart';
 _AssetModel _$AssetModelFromJson(Map<String, dynamic> json) => _AssetModel(
   coinId: json['coinId'] as String,
   symbol: json['symbol'] as String,
-  amount: const DecimalConverter().fromJson(json['amount']),
+  amount: const _StringDecimalConverter().fromJson(json['amount']),
 );
 
 Map<String, dynamic> _$AssetModelToJson(_AssetModel instance) =>
     <String, dynamic>{
       'coinId': instance.coinId,
       'symbol': instance.symbol,
-      'amount': const DecimalConverter().toJson(instance.amount),
+      'amount': const _StringDecimalConverter().toJson(instance.amount),
     };
