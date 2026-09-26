c64f2d8bfe70e14db88b2d82ecfbf11939963405
Jeremiah <jeremiaholuwafemi16@gmail.com>
2026-09-26 18:02:21 +0100

    chore: switch default themeMode to dark

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class CryptoSimApp extends ConsumerWidget {
       title: 'CryptoSim',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.light,
+      themeMode: ThemeMode.dark,
       routerConfig: router,
       debugShowCheckedModeBanner: false,
       builder: (context, child) {
